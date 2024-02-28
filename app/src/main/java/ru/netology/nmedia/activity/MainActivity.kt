e94c287c0cc801a87bb6916a1422fd67edb3a424
JuliaZank <144544513+JuliaZank@users.noreply.github.com>
2024-02-28 23:12:38 +0300

    Предыдущая реализация переделена на MVVM

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -24,29 +24,18 @@ class MainActivity : AppCompatActivity() {
                 published.text = post.published
                 text.text = post.text
 
+                likes.setImageResource(if(post.likedByMe) R.drawable.baseline_favorite_24 else R.drawable.baseline_favorite_border_24)
+
                 calculate(post.likesCount, likesCount)
                 calculate(post.shareCount, shareCount)
                 calculate(post.viewsCount, viewsCount)
-
-                if (post.likedByMe) {
-                    likes.setImageResource(R.drawable.baseline_favorite_24)
-                }
-
-                likes.setOnClickListener {
-                    if (post.likedByMe) post.likesCount-- else post.likesCount++
-                    post.likedByMe = !post.likedByMe
-                    likes.setImageResource(if (post.likedByMe) R.drawable.baseline_favorite_24 else R.drawable.baseline_favorite_border_24)
-                    calculate(post.likesCount, likesCount)
-                }
-
-                share.setOnClickListener {
-                    if (post.sharedByMe) post.shareCount++
-                    calculate(post.shareCount, shareCount)
-                }
             }
-            }
+        }
         binding.likes.setOnClickListener {
             viewModel.like()
+        }
+        binding.share.setOnClickListener {
+            viewModel.share()
         }
     }
 }
